7e2c7509d677ce4fb899f87fada32deed580f821
rulicimot <konigsblauen@gmail.com>
2014-07-01 16:33:54 +0700

    code consistency

--- index.js
+++ index.js
@@ -19,52 +19,52 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // catch all route to set global values
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.type('application/json');
 	res.locals.wrap = wrapper.create();
 	next();
 });
 
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.send("The API is working");
 });
 // get movie list
-app.get('/movies', function(req, res) {
-	db.movies.find({}, function(err, results) {
-		if(err) {
-			res.json(500, { error:err });
+app.get('/movies', function (req, res) {
+	db.movies.find({}, function (err, results) {
+		if (err) {
+			res.json(500, { error: err });
 			return;
 		}
-		res.json(200, res.locals.wrap(results.map(function(movie) {
+		res.json(200, res.locals.wrap(results.map(function (movie) {
 			movie.links = { self: root + '/movies/' + movie._id };
 			return movie;
 		})));
 	});
 });
 // insert movie
-app.post('/movies', function(req, res) {
-	if(!req.body.title) {
-		res.json(400, { error: { message: "A title is required to create a new movie." }} );
+app.post('/movies', function (req, res) {
+	if (!req.body.title) {
+		res.json(400, { error: { message: "A title is required to create a new movie." }});
 		return;
 	}
-	db.movies.insert({title: req.body.title}, function(err, created) {
-		if(err) {
-			res.json(500, { error:err });
+	db.movies.insert({title: req.body.title}, function (err, created) {
+		if (err) {
+			res.json(500, { error: err });
 			return;
 		}
-		res.set('Location', root + '/movies/' + created._id)
+		res.set('Location', root + '/movies/' + created._id);
 		res.json(201, created);
 	});
 });
 // get a movie
-app.get('/movies/:id', function(req, res) {
-	db.movies.findOne({ _id: req.params.id }, function(err, result) {
-		if(err) {
+app.get('/movies/:id', function (req, res) {
+	db.movies.findOne({ _id: req.params.id }, function (err, result) {
+		if (err) {
 			res.json(500, { error: err });
 			return;
 		}
-		if(!result) {
+		if (!result) {
 			res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
 			return;
 		}
@@ -72,19 +72,19 @@ app.get('/movies/:id', function(req, res) {
 	});
 });
 // update movie
-app.put('/movies/:id', function(req, res) {
-	db.movies.update({ _id: req.params.id }, req.body, function(err, num) {
+app.put('/movies/:id', function (req, res) {
+	db.movies.update({ _id: req.params.id }, req.body, function (err, num) {
 		res.locals.respond(err, { success: num + " records updated"});
 	});
 });
 // delete movie
-app.delete('/movies/:id', function(req, res) {
-	db.movies.remove({ _id: req.params.id }, function(err, num) {
-		if(err) {
-			res.json(500, { error:err });
+app.delete('/movies/:id', function (req, res) {
+	db.movies.remove({ _id: req.params.id }, function (err, num) {
+		if (err) {
+			res.json(500, { error: err });
 			return;
 		}
-		if(num === 0) {
+		if (num === 0) {
 			res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
 			return;
 		}
